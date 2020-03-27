17e8399b3ecc726029883e2308b87fe2fd31bdc7
Glitch (hello-express) <none>
2020-03-27 16:11:22 +0000

    📺🏠 Checkpoint ./public/encoder-worker.js:577339/706 ./public/test-audio.html:577339/378

--- public/encoder-worker.js
+++ public/encoder-worker.js
@@ -1,4 +1,11 @@
+console.log('init')
 self.OggVorbisEncoderConfig = {
-  memoryInitializerPrefixURL: "javascripts/memory/"
+  memoryInitializerPrefixURL: "https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/"
 };
-importScripts("javascripts/OggVorbisEncoder.min.js");
+importScripts("https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/OggVorbisEncoder.min.js");
+console.log('imported')
+onmessage = (e) => {
+  var encoder = new OggVorbisEncoder(44100, 2, 0.3)
+  encoder.encode(e.data.audioData)
+  postMessage({ blob: encoder.finish() })
+}

5cdbdf2de06d3e187de57fc2ef7019e6416c3580
Glitch (hello-express) <dtinth@spacet.me>
2020-03-27 17:21:25 +0000

    🏢👭 Checkpoint ./public/recorder-worklet.js:577339/1573 ./public/index.html:577339/240

--- public/recorder-worklet.js
+++ public/recorder-worklet.js
@@ -7,14 +7,42 @@ class RecorderProcessor extends AudioWorkletProcessor {
   }
   constructor() {
     super()
-    this.currentRecordId = -1
+    this.currentRecording = null
   }
   process([input], outputs, { recordId }) {
+    console.log('whee')
     if (recordId.length === 1) {
-      this.save(recordId[0], )
+      this.save(recordId[0], input.map(a => a.slice()))
     } else {
+      let current
       for (let i = 0; i < recordId.length; i++) {
+        if (!current || recordId[i] !== current.recordId) {
+          if (current) {
+            this.save(current.recordId, input.map(a => a.slice(current.startIndex, i)))
+          }
+          current = {
+            recordId: recordId[i],
+            startIndex: i
+          }
+        }
       }
+      if (current) {
+        this.save(current.recordId, input.map(a => a.slice(current.startIndex)))
+      }
+    }
+    return true
+  }
+  save(recordId, chunk) {
+    if (this.currentRecording && recordId !== this.currentRecording.recordId) {
+      this.port.postMessage(this.currentRecording)
+      this.currentRecording = null
+    }
+    if (!this.currentRecording && recordId !== -1) {
+      this.currentRecording = { recordId, chunks: [], totalLength: 0 }
+    }
+    if (this.currentRecording) {
+      this.currentRecording.chunks.push(chunk)
+      this.currentRecording.totalLength += chunk[0].length
     }
   }
 }

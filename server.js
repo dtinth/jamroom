5d4d07f4563b012730b979ca38a198161c371384
Glitch (hello-express) <none>
2020-03-27 16:23:23 +0000

    👭💵 Checkpoint ./package.json:577339/1285 ./server.js:577339/513 ./public/index.html:577339/3205

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  socket.on('jam', () => {
+    socket.emit('ready to jam', {
+      currentTime: Date.now(),
+    })
+  })
+})
 
 app.use(express.static("public"));
 
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+server.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + server.address().port);
 });

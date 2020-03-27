42cc726211c9364231137b79520532d84831a046
Glitch (hello-express) <dtinth@spacet.me>
2020-03-27 17:45:27 +0000

    🌞🙌 Checkpoint ./public/index.html:577339/4188 ./public/encoder-worker.js:577339/616

--- public/encoder-worker.js
+++ public/encoder-worker.js
@@ -4,8 +4,12 @@ self.OggVorbisEncoderConfig = {
 };
 importScripts("https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/OggVorbisEncoder.min.js");
 onmessage = (e) => {
-  var encoder = new OggVorbisEncoder(e.data.sampleRate, e.data.numberOfChannels, 0.3)
-  for (const chunk of e.data.audioData) encoder.encode(chunk)
-  postMessage({ blob: encoder.finish() })
+  if (e.data.hello) {
+    postMessage({ ready: true })
+  } else if (e.data.audioData) {
+    var encoder = new OggVorbisEncoder(e.data.sampleRate, e.data.numberOfChannels, 0.3)
+    for (const chunk of e.data.audioData) encoder.encode(chunk)
+    postMessage({ blob: encoder.finish(), id: e.id })
+  }
 }
 console.log('[encoder-worker] ready')

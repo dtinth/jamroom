532eb8fd1656fdd2f587ca161e68d4aa85e6f649
Glitch (hello-express) <dtinth@spacet.me>
2020-03-28 08:17:11 +0000

    🍤📯 Checkpoint ./public/index.html:577339/35564 ./.prettierrc:577339/42 ./public/encoder-worker.js:577339/1529

--- public/encoder-worker.js
+++ public/encoder-worker.js
@@ -1,14 +1,21 @@
 /* global OggVorbisEncoder */
 console.log('[encoder-worker] initializing')
 self.OggVorbisEncoderConfig = {
-  memoryInitializerPrefixURL: "https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/"
-};
-importScripts("https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/OggVorbisEncoder.min.js");
-onmessage = (e) => {
+  memoryInitializerPrefixURL:
+    'https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/'
+}
+importScripts(
+  'https://rawcdn.githack.com/higuma/ogg-vorbis-encoder-js/7a872423f416e330e925f5266d2eb66cff63c1b6/lib/OggVorbisEncoder.min.js'
+)
+onmessage = e => {
   if (e.data.hello) {
     postMessage({ ready: true })
   } else if (e.data.audioData) {
-    var encoder = new OggVorbisEncoder(e.data.sampleRate, e.data.numberOfChannels, 0.3)
+    var encoder = new OggVorbisEncoder(
+      e.data.sampleRate,
+      e.data.numberOfChannels,
+      0.3
+    )
     for (const chunk of e.data.audioData) encoder.encode(chunk)
     postMessage({ blob: encoder.finish(), id: e.data.id })
   }
